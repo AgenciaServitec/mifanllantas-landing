5584f11996435f53c50bc24742a9ed545e2c557b
Angel Gala <galafloresangelemilio@gmail.com>
2024-04-19 13:35:31 -0500

    add contact section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,5 @@
 export { default as ProductsSection } from "./ProductsSection.astro";
 export { default as MapLocation } from "./MapLocation.astro";
-export { default as DeliverySection } from "./DeliverySection.astro";
+export { default as ContactSection } from "./ContactSection.astro";
 export { default as Testimonials } from "./Testimonials.astro";
 export { default as AboutUsComponent } from "./AboutUs.astro";

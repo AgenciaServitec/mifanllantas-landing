67204f6db93337408c76e963dcc3bb42b2a9914a
Angel Gala <galafloresangelemilio@gmail.com>
2024-04-24 19:45:52 -0500

    add adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import {seoConfig} from "./src/utils/seoConfig"
+import { seoConfig } from "./src/utils/seoConfig";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   build: {
-    inlineStylesheets: "always",
+    inlineStylesheets: "always"
   },
   compressHTML: true,
   prefetch: true,
   output: "hybrid",
   site: seoConfig.baseURL,
+  adapter: netlify()
 });

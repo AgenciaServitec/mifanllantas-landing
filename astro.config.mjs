780ce7598dc9db070096e6ba3a5aed583d5ef634
Angel Gala <galafloresangelemilio@gmail.com>
2024-05-20 12:44:06 -0500

    added partytown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,24 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import { seoConfig } from "./src/utils/seoConfig";
 import netlify from "@astrojs/netlify/functions";
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   build: {
-    inlineStylesheets: "always"
+    inlineStylesheets: "always",
   },
   compressHTML: true,
   prefetch: true,
   output: "server",
   site: seoConfig.baseURL,
-  adapter: netlify()
+  adapter: netlify(),
 });
